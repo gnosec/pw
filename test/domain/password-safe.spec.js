5ab88a0591305d416ab06d58c13dbf0eb7f34d3c
gnosec <gnosec@protonmail.com>
2018-03-18 22:39:27 -0700

    adds tests

--- test/domain/password-safe.spec.js
+++ test/domain/password-safe.spec.js
@@ -2,35 +2,200 @@ const assert = require('assert');
 
 const PasswordSafe = require('../../lib/domain/password-safe');
 
-const passwordSafe = new PasswordSafe({
-    'one.two.three.four.five': 1,
-    'one.two.three.four': 1,
-    'one.two.three': 1,
-    'one.two': 1,
-    'one': 1
-});
-
 describe('PasswordSafe', () => {
 
-    describe('#getConflicts()', () => {
-        it('should return -1 when the value is not present', () => {
-            // assert.equal([1, 2, 3].indexOf(4), -1);
+    const InvalidPaths = [
+        '',
+        null,
+        undefined,
+        999,
+        {},
+        [],
+        () => {},
+        '/leadingslash',
+        'tailingslash/',
+        'multiple//consecutive//slashes',
+        'has space',
+        'has\ttabs',
+        'has\nnewlines'
+    ];
+
+    const InvalidValues = [
+        null,
+        undefined,
+        999,
+        {},
+        [],
+        () => {}
+    ];
+
+    describe('set()', () => {
+
+        it('should throw error for invalid paths', () => {
+            InvalidPaths.forEach(invalidPath => {
+                expect(() => passwordSafe.set(invalidPath, 'validValue')).toThrowError();
+            })
         });
-        // it('should return all conflicts when calling getConflicts()', function() {
 
-        // expect(passwordSafe.getConflicts('one').toEqual([
+        it('should throw error for invalid values', () => {
+            InvalidValues.forEach(invalidValue => {
+                expect(() => passwordSafe.set('validPath', invalidValue)).toThrowError();
+            })
+        });
 
-        // ]))
-        // })
+        it('should set the specified value', () => {
+            const data = {};
+            const passwordSafe = new PasswordSafe(data);
+
+            passwordSafe.set('key', 'value');
+            expect(data.key).toBe('value');
+        });
+
+        it('should overwrite existing values if keys match', () => {
+            const data = {key: '1'};
+            const passwordSafe = new PasswordSafe(data);
+
+            passwordSafe.set('key', '2');
+            expect(data.key).toBe('2');
+        });
+
+        it('should not set the value when a conflict exists', () => {
+            const passwordSafe = new PasswordSafe({
+                'one/2': 1,
+                'one/two': 1,
+                'other': 1
+            });
+            expect(() => passwordSafe.set('one/two/three', 'value')).toThrowError();
+            expect(() => passwordSafe.set('one', 'value')).toThrowError();
+        });
+
     });
 
-    describe('#getMatches()', () => {
+    describe('get()', () => {
 
-        it('should return all matches when calling getMatches()', () => {
+        it('should throw error for invalid paths', () => {
+            InvalidPaths.forEach(invalidPath => {
+                expect(() => passwordSafe.get(invalidPath, 'validValue')).toThrowError();
+            })
+        });
 
-        })
+        it('should get the specified value', () => {
+            const data = {key: 'value'};
+            const passwordSafe = new PasswordSafe(data);
+
+            expect(passwordSafe.get('key', 'value')).toBe(data.key);
+        });
+
+        it('should return undefined when it does not match', () => {
+            const passwordSafe = new PasswordSafe({
+                'one/2': 1,
+                'one/two': 1,
+                'other': 1
+            });
+
+            expect(passwordSafe.get('missing')).toBeUndefined();
+            expect(passwordSafe.get('one')).toBeUndefined();
+            expect(passwordSafe.get('one/two/three')).toBeUndefined();
+        });
+
     });
 
-    
+    describe('delete()', () => {
+
+        it('should throw error for invalid paths', () => {
+            InvalidPaths.forEach(invalidPath => {
+                expect(() => passwordSafe.delete(invalidPath, 'validValue')).toThrowError();
+            })
+        });
+
+        it('should throw error if there is no matching key', () => {
+            const passwordSafe = new PasswordSafe({
+                'key': 1
+            });
+            expect(() => passwordSafe.get('wrongKey')).toThrowError();
+        });
+
+        it('should delete the specified value', () => {
+            const data = {key: 'value'};
+            const passwordSafe = new PasswordSafe(data);
+            passwordSafe.delete('key')
+            expect(data.key).toBeUndefined();
+        });
+
+        it('should delete all matches', () => {
+            const data = {
+                'match/one': 1,
+                'match/two': 1,
+                'notAMatch': 1
+            };
+            const passwordSafe = new PasswordSafe(data);
+            passwordSafe.delete('match');
+            expect(data['match/one']).toBeUndefined();
+            expect(data['match/two']).toBeUndefined();
+            expect(data['notAMatch']).toBeDefined();
+        });
+
+    });
+
+    describe('getSetConflicts()', () => {
+
+        const passwordSafe = new PasswordSafe({
+            'one/2': 1,
+            'one/two': 1,
+            'other': 1
+        });
+
+        it('should throw error for invalid paths', () => {
+            InvalidPaths.forEach(invalidPath => {
+                expect(() => passwordSafe.getSetConflicts(invalidPath)).toThrowError();
+            })
+        });
+
+        it('should not return exact matches', () => {
+            expect(passwordSafe.getSetConflicts('one/two'))
+                .toEqual([]);
+        });
+
+        it('should return all keys with matching ancestor directory', () => {
+            expect(passwordSafe.getSetConflicts('one').sort())
+                .toEqual(['one/2', 'one/two'].sort())
+        });
+
+        it('should return matching ancestor keys', () => {
+            expect(passwordSafe.getSetConflicts('one/two/three'))
+                .toEqual(['one/two'])
+        });
+    });
+
+    describe('getDeleteMatches()', () => {
+
+        const passwordSafe = new PasswordSafe({
+            'one/2': 1,
+            'one/two': 1,
+            'other': 1
+        });
+
+        it('should throw error for invalid paths', () => {
+            InvalidPaths.forEach(invalidPath => {
+                expect(() => passwordSafe.getDeleteMatches(invalidPath)).toThrowError();
+            })
+        });
+
+        it('should return exact matches', () => {
+            expect(passwordSafe.getDeleteMatches('one/two'))
+                .toEqual(['one/two']);
+        });
+
+        it('should return all keys with matching ancestor directory', () => {
+            expect(passwordSafe.getDeleteMatches('one').sort())
+                .toEqual(['one/2', 'one/two'].sort())
+        });
+
+        it('should not return matching ancestor keys', () => {
+            expect(passwordSafe.getDeleteMatches('one/two/three'))
+                .toEqual([])
+        });
+
+    });
 
 })
